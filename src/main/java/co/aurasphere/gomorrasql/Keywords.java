cfaf6f7be718801c61f7c0155fef86a0e9e9b150
Matteo Baccan <matteo.baccan@gmail.com>
2021-05-07 11:59:35 +0200

    Correct modifiers order

--- src/main/java/co/aurasphere/gomorrasql/Keywords.java
+++ src/main/java/co/aurasphere/gomorrasql/Keywords.java
@@ -11,25 +11,26 @@ import java.util.List;
  */
 public class Keywords {
 
-	public final static String SELECT_KEYWORD = "ripigliammo";
-	public final static String UPDATE_KEYWORD = "rifacimm";
-	public final static String[] INSERT_KEYWORDS = { "nzipp", "'ngoppa" };
+	public static final String SELECT_KEYWORD = "ripigliammo";
+	public static final String UPDATE_KEYWORD = "rifacimm";
+	public static final String[] INSERT_KEYWORDS = { "nzipp", "'ngoppa" };
 	public static final String[] DELETE_KEYWORDS = { "facimm", "na'", "strage" };
 	public static final String[] JOIN_KEYWORDS = { "pesc", "e", "pesc" };
 	public static final String[] FROM_KEYWORDS = { "mmiez", "'a" };
-	public final static String[] ASTERISK_KEYWORDS = { "tutto", "chillo", "ch'era", "'o", "nuostro" };
-	public final static String WHERE_KEYWORD = "arò";
-	public final static String[] BEGIN_TRANSACTION_KEYWORDS = { "ua", "uagliò" };
-	public final static String[] COMMIT_KEYWORDS = { "iamme", "bello", "ia'" };
-	public final static String ROLLBACK_KEYWORD = "sfaccimm";
-	public final static String AND_KEYWORD = "e";
-	public final static String OR_KEYWORD = "o";
-	public final static String NULL_KEYWORD = "nisciun";
-	public final static String IS_KEYWORD = "è";
-	public final static String VALUES_KEYWORD = "chist";
-	public final static String[] IS_NOT_KEYWORDS = { "nun", "è" };
-	public final static String SET_KEYWORD = "accunza";
-	public final static List<String> WHERE_OPERATORS = Arrays.asList(">", "<", "=", "!=", "<>", ">=", "<=",
+	public static final String[] ASTERISK_KEYWORDS = { "tutto", "chillo", "ch'era", "'o", "nuostro" };
+	public static final String WHERE_KEYWORD = "arò";
+	public static final String[] BEGIN_TRANSACTION_KEYWORDS = { "ua", "uagliò" };
+	public static final String[] COMMIT_KEYWORDS = { "iamme", "bello", "ia'" };
+	public static final String ROLLBACK_KEYWORD = "sfaccimm";
+	public static final String AND_KEYWORD = "e";
+	public static final String OR_KEYWORD = "o";
+	public static final String NULL_KEYWORD = "nisciun";
+	public static final String IS_KEYWORD = "è";
+	public static final String VALUES_KEYWORD = "chist";
+	public static final String[] IS_NOT_KEYWORDS = { "nun", "è" };
+	public static final String SET_KEYWORD = "accunza";
+	public static final List<String> WHERE_OPERATORS = Arrays.asList(">", "<", "=", "!=", "<>", ">=", "<=",
 			Keywords.IS_KEYWORD, Keywords.IS_NOT_KEYWORDS[0]);
-	public final static String SET_EQUAL_KEYWORD = "accussì";
+	public static final String SET_EQUAL_KEYWORD = "accussì";
+        public static final String LIMIT_KEYWORD = "è pccirill ashpiett";
 }
